fd63fb94c525aa58ddd60895e8540e87ea729575
Stacey T <stacey.troy.5@gmail.com>
2016-05-04 13:46:19 -0400

    update formatting

--- src/learner.py
+++ src/learner.py
@@ -10,7 +10,6 @@ def add_team_vote(*args, **kwargs):
 
     with open('LunchSpotData.txt', 'a') as lunch_data_fp:
         for restaurant in args:
-            print restaurant
             entry += (',' + restaurant)
         entry += '\n'
         lunch_data_fp.write(entry)
@@ -29,7 +28,7 @@ def seed_data(number_of_teammates):
                 lunch_spot_features[restaurant_info[0]].append(restaurant_info[index])
 
     start_timestamp = datetime.now() - relativedelta(months=3)
-    for relative_days in range(30*3):
+    for relative_days in range(30 * 3):
         entry_date = start_timestamp + relativedelta(days=relative_days)
         random_team_votes = []
         for team_vote in range(number_of_teammates):
@@ -39,39 +38,42 @@ def seed_data(number_of_teammates):
 
 
 def get_two_choices():
-	lunch_spot_pool = []
-	lunch_spot_features = defaultdict(list)
+    lunch_spot_pool = []
+    lunch_spot_features = defaultdict(list)
 
-	# add tally for each restaurant
-	with open('LunchSpotFeatures.csv', 'r') as lunch_feature_fp:
-		for line in lunch_feature_fp:
-			restaurant_info = line.split(',')
-			lunch_spot_pool.append(restaurant_info[0])
-			for index in range(1, len(restaurant_info)):
-				lunch_spot_features[restaurant_info[0]].append(restaurant_info[index])
+    # add tally for each restaurant
+    with open('LunchSpotFeatures.csv', 'r') as lunch_feature_fp:
+        for line in lunch_feature_fp:
+            restaurant_info = line.replace('\n', '').split(',')
+            lunch_spot_pool.append(restaurant_info[0])
+            for index in range(1, len(restaurant_info)):
+                lunch_spot_features[restaurant_info[0]].append(restaurant_info[index])
 
-	# skew restaurant decision toward resturants with more votes
-	with open('LunchSpotData.txt', 'r') as lunch_data_fp:
-		for line in lunch_data_fp:
-			daily_restaurant_decisions = line.split(',')
-			if datetime.strptime(daily_restaurant_decisions[0], '%x') - relativedelta(months=3) < datetime.now():
-				for index in range(1, len(daily_restaurant_decisions)):
-					lunch_spot_pool.append(daily_restaurant_decisions[index])
+    # skew restaurant decision toward resturants with more votes
+    with open('LunchSpotData.txt', 'r') as lunch_data_fp:
+        for line in lunch_data_fp:
+            daily_restaurant_decisions = line.split(',')
+            if datetime.strptime(daily_restaurant_decisions[0], '%x') - relativedelta(months=3) < datetime.now():
+                for index in range(1, len(daily_restaurant_decisions)):
+                    lunch_spot_pool.append(daily_restaurant_decisions[index])
 
-	random.shuffle(lunch_spot_pool)
+    random.shuffle(lunch_spot_pool)
 
-	# select first choice, get set of attributes
-	first_choice = random.choice(lunch_spot_pool)
-	first_choice_attributes = set(lunch_spot_features[first_choice])
-	print first_choice
+    # select first choice, get set of attributes
+    first_choice = random.choice(lunch_spot_pool)
+    first_choice_attributes = set(lunch_spot_features[first_choice])
 
-	# get second choice where no attributes overlap
-	second_choice = random.choice(lunch_spot_pool)
-	second_choice_attributes = set(lunch_spot_features[second_choice])
-	while (first_choice_attributes.intersection(second_choice_attributes)):
-		second_choice = random.choice(lunch_spot_pool)
-		second_choice_attributes = set(lunch_spot_features[second_choice])
+    # get second choice where no attributes overlap
+    second_choice = random.choice(lunch_spot_pool)
+    second_choice_attributes = set(lunch_spot_features[second_choice])
+    print first_choice_attributes.intersection(second_choice_attributes)
 
-	print second_choice
+    while (first_choice_attributes.intersection(second_choice_attributes)):
+        second_choice = random.choice(lunch_spot_pool)
+        second_choice_attributes = set(lunch_spot_features[second_choice])
+        print first_choice_attributes.intersection(second_choice_attributes)
+
+    print first_choice
+    print second_choice
 
 get_two_choices()
